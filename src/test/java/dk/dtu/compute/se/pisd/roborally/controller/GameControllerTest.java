8640e1e8b3795852b53194ca36bac106e11be492
Lindblad <mathias@aslindblad.com>
2023-04-18 13:29:27 +0200

    made changes to the move code, so that players will be pushed the right direction when backed into

--- src/test/java/dk/dtu/compute/se/pisd/roborally/controller/GameControllerTest.java
+++ src/test/java/dk/dtu/compute/se/pisd/roborally/controller/GameControllerTest.java
@@ -39,8 +39,8 @@ class GameControllerTest {
         Board board = gameController.board;
         Player player1 = board.getPlayer(0);
         Player player2 = board.getPlayer(1);
-
-        gameController.moveCurrentPlayerToSpace(board.getSpace(0, 4));
+        boolean backupflag = false;
+        gameController.moveCurrentPlayerToSpace(board.getSpace(0, 4), backupflag);
 
         Assertions.assertEquals(player1, board.getSpace(0, 4).getPlayer(), "Player " + player1.getName() + " should beSpace (0,4)!");
         Assertions.assertNull(board.getSpace(0, 0).getPlayer(), "Space (0,0) should be empty!");
