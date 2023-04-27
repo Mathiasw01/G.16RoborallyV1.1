4ab62af69819f5076831fc9a8db3ac53edc9505c
MathiasW <s224293@student.dtu.dk>
2023-04-27 16:39:07 +0200

    Started making a "physical" deck of cards

---
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/ProgrammingDeckInit.java
@@ -0,0 +1,36 @@
+package dk.dtu.compute.se.pisd.roborally.model;
+
+import java.util.List;
+
+public class ProgrammingDeckInit {
+    private List<CommandCard> deck;
+
+    public ProgrammingDeckInit() {
+    }
+
+
+    public List<CommandCard> init(){
+        deck.add(new CommandCard(Command.AGAIN));
+        deck.add(new CommandCard(Command.UTURN));
+        deck.add(new CommandCard(Command.LEFT));
+        deck.add(new CommandCard(Command.LEFT));
+        deck.add(new CommandCard(Command.LEFT));
+        deck.add(new CommandCard(Command.RIGHT));
+        deck.add(new CommandCard(Command.RIGHT));
+        deck.add(new CommandCard(Command.RIGHT));
+        deck.add(new CommandCard(Command.MOVE_BACK));
+        deck.add(new CommandCard(Command.FORWARD));
+        deck.add(new CommandCard(Command.FORWARD));
+        deck.add(new CommandCard(Command.FORWARD));
+        deck.add(new CommandCard(Command.FORWARD));
+        deck.add(new CommandCard(Command.FORWARD));
+        deck.add(new CommandCard(Command.FAST_FORWARD));
+        deck.add(new CommandCard(Command.FAST_FORWARD));
+        deck.add(new CommandCard(Command.FAST_FORWARD));
+        deck.add(new CommandCard(Command.MOVE_THREE));
+        deck.add(new CommandCard(Command.POWERUP));
+        return deck;
+    }
+
+
+}
